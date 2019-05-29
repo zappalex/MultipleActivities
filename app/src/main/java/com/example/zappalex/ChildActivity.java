187655a1a68f798db7b2b58c7b4d15151b59bf48
User <alexashworth88@gmail.com>
2019-05-29 15:57:32 -0400

    add textview register method

--- app/src/main/java/com/example/zappalex/ChildActivity.java
+++ app/src/main/java/com/example/zappalex/ChildActivity.java
@@ -6,15 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
 
     private static final String TAG = "ChildActivity";
+    private TextView messageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        registerTextView();
+    }
+
+    private void registerTextView() {
+        messageTextView = findViewById(R.id.message_textview);
     }
 
 }
